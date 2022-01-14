01b6bfef7e5cd610f487ef36ab1a438817838709
4YCAaaa <79677971+4YCAaaa@users.noreply.github.com>
2022-01-14 11:34:12 +0800

    Update render.js

--- SomeTryyy/render.js
+++ SomeTryyy/render.js
@@ -2,65 +2,67 @@ export function layout(title, content) {
   return `
   <html>
   <head>
+  <link rel="stylesheet" href="https://unpkg.com/sakura.css/css/sakura.css" type="text/css">
+
     <title>${title}</title>
     <style>
-      body {
-        padding: 60px;
-        font: 16px ;
+    * {
+        box-sizing: border-box;
+      }
+      body 
+      {
+        margin: 0;
       }
   
-      h1,h2 {
-        font-size: 2px;
-      }
-  
-      #posts {
+      #posts 
+      {
         margin: 0;
         padding: 0;
       }
   
-      #posts li {
+      #posts li 
+      {
         margin: 40px 0;
-        padding-bottom: 15px;
-        border-bottom: 1.5px solid #eee;
+        padding: 0;
+        padding-bottom: 20px;
+        border-bottom: 1px solid #eee;
         list-style: none;
       }
   
-      #posts li:last-child {
+      #posts li:last-child 
+      {
         border-bottom: none;
       }
   
       textarea {
         width: 500px;
-        height: 350px;
+        height: 300px;
       }
-  
-      input[type=text],
+
       textarea {
-        border: 1px solid ;
-        border-radius: 10px;
+        border: 1px solid #aaa;
+        border-top-color: #bbb;
+        border-left-color: #ccc;
+        border-radius: 2px;
         padding: 15px;
-        font-size: 8px;
+        font-size: .8em;
       }
   
       input[type=text] {
-        width: 300px;
+        width: 500px;
       }
+
+      input[type=text],
+
     </style>
   </head>
   <body>
-    <section id="content">
-      ${content}
-    </section>
+          <section id="content">
+              ${content}
+          </section>
   </body>
   </html>
   `
-}
-
-export function show(post) {
-  return layout(post.title, `
-    <h1>${post.title}</h1>
-    <pre>${post.body}</pre>
-  `)
 }
 
 export function list(posts) {
@@ -68,30 +70,43 @@ export function list(posts) {
   for (let post of posts) {
     list.push(`
     <li>
-      <h2>${ post.title }</h2>
-      <p><a href="/post/${post.id}">Read post</a></p>
+      <h2>${post.title}</h2>
+      <p>Date ${post.date}</p>
+      <p><a href="/post/${post.id}">Read</a></p>
     </li>
     `)
   }
+
   let content = `
-  <h1>Posts</h1>
-  <p>You have <strong>${posts.length}</strong> posts!</p>
-  <p><a href="/post/new">Create a Post</a></p>
-  <ul id="posts">
-    ${list.join('\n')}
-  </ul>
+  <h1>Post</h1>
+  <form action="/post/new" method="post"> 
+      <input type="submit" value="New">
+  </form>     
+  <div>
+      <ul id="posts">
+      ${list.join('\n')}
+      </ul>
+  </div>
   `
   return layout('Posts', content)
 }
 
 export function newPost() {
   return layout('New Post', `
-  <h1>New Post</h1>
-  <p>Create a new post.</p>
-  <form action="/post" method="post">
-    <p><input type="text" placeholder="Title" name="title"></p>
+  <h1>New</h1>
+  <form action="/post" method="post">  
+    <p><input type="text" placeholder="title" name="title"></p>
+    <p><input type="date" placeholder="XXXX/XX/XX" name="date"></p>
     <p><textarea placeholder="Contents" name="body"></textarea></p>
     <p><input type="submit" value="Create"></p>
   </form>
+  `)
+}
+
+export function show(post) {
+  return layout(post.title, `
+    <h1>${post.title}</h1>
+    <p> ${post.date}</p>
+    <p>${post.body}</p>
   `)
 }
